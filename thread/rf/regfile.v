ce2b71ab809953940b5a8b6c8a5b765918b7eddd
Mihir-Divyansh <jee.emd2005@gmail.com>
2025-05-21 10:42:32 +0530

    Added dont touch to reg file, added simd

--- thread/rf/regfile.v
+++ thread/rf/regfile.v
@@ -6,25 +6,28 @@ module registerFile
     input wire [3:0] rs1,
     input wire [55:0] data_in,
     input wire [3:0] dest_sel,
-    input wire isSIMD,
+    input wire isrdSIMD,
+    input wire iswrSIMD,
     input wire wen,
     output wire [27:0] dout0,
     output wire [27:0] dout1
 );
 
-    (* dont_touch = "yes" *) reg [27:0] rf [1:15]; 
+    (* dont_touch = "yes" *) reg [27:0] rf [0:15]; 
     integer i;
 
-    assign dout0 = rf[rs0];
-    assign dout1 = rf[rs1];
+    assign dout0 = isrdSIMD ? {rf[rs0], rf[rs0 + 1]} : {rf[rs0], 28'b0};
+    assign dout1 = isrdSIMD ? {rf[rs1], rf[rs1 + 1]} : {rf[rs1], 28'b0};
 
     always @(negedge clk or posedge rst) begin
         if (rst) begin
             for (i = 0; i < 16; i = i + 1) begin
                 rf[i] <= 28'b0;
             end
-        end else begin
-            if (wen) rf[dest_sel] <= data_in;
+        end else if (wen and iswrSIMD) begin
+            {rf[dest_sel], rf[dest_sel + 1]} <= data_in;
+        end else if (wen) begin
+            rf[dest_sel] <= data_in[55:28];
         end
     end
 endmodule
